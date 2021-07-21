305929235a7397a95c55b6c937af4b7ec0f3f6fd
Rodrigo Arias <rodrigoarias4@gmail.com>
2021-07-21 11:55:57 -0700

    added postgres server

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,15 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-  logging: false
-});
+const user = "postgres";
+
+const pass = "pagedown4";
+
+const db = new Sequelize(
+  process.env.DATABASE_URL ||
+    `postgres://${user}:${pass}@localhost:5432/messenger`,
+  {
+    logging: false,
+  }
+);
 
 module.exports = db;
