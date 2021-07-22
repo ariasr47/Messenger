ab5e5d58268b1698b658277feff971a68c0999c8
Rodrigo Arias <rodrigoarias4@gmail.com>
2021-07-22 16:40:30 -0700

    removed db info from db.js

--- server/db/db.js
+++ server/db/db.js
@@ -1,15 +1,9 @@
 const Sequelize = require("sequelize");
 
-const user = "postgres";
+console.log(process.env.DATABASE_URL);
 
-const pass = "pagedown4";
-
-const db = new Sequelize(
-  process.env.DATABASE_URL ||
-    `postgres://${user}:${pass}@localhost:5432/messenger`,
-  {
-    logging: false,
-  }
-);
+const db = new Sequelize(process.env.DATABASE_URL, {
+  logging: false,
+});
 
 module.exports = db;
