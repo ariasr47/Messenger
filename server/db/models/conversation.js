4f04aa66ad875e8f3e9c92b26c7c4602978191f9
Rodrigo Arias <rodrigoarias4@gmail.com>
2021-07-22 16:29:20 -0700

    fixed *not being able to properly send messages*, fixed *messsages not displayed in proper order*, fixed *security issue related to sending messsages*

--- server/db/models/conversation.js
+++ server/db/models/conversation.js
@@ -1,6 +1,5 @@
 const { Op } = require("sequelize");
 const db = require("../db");
-const Message = require("./message");
 
 const Conversation = db.define("conversation", {});
 
@@ -10,15 +9,25 @@ Conversation.findConversation = async function (user1Id, user2Id) {
   const conversation = await Conversation.findOne({
     where: {
       user1Id: {
-        [Op.or]: [user1Id, user2Id]
+        [Op.or]: [user1Id, user2Id],
       },
       user2Id: {
-        [Op.or]: [user1Id, user2Id]
-      }
-    }
+        [Op.or]: [user1Id, user2Id],
+      },
+    },
   });
 
   // return conversation or null if it doesn't exist
+  return conversation;
+};
+
+Conversation.getConversationById = async function (conversationId) {
+  const conversation = await Conversation.findOne({
+    where: {
+      id: conversationId,
+    },
+  });
+
   return conversation;
 };
 
